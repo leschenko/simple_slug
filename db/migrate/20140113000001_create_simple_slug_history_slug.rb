9f7081c5791579e0ea76c11c68b6c5288c281e07
Alex Leschenko <leschenko.al@gmail.com>
2019-04-05 15:53:44 +0300

    Support utf8mb4 in migration

--- db/migrate/20140113000001_create_simple_slug_history_slug.rb
+++ db/migrate/20140113000001_create_simple_slug_history_slug.rb
@@ -1,13 +1,13 @@
 class CreateSimpleSlugHistorySlug < ActiveRecord::Migration
   def change
-    create_table :simple_slug_history_slug do |t|
-      t.string :slug, null: false
+    create_table :simple_slug_history_slugs do |t|
+      t.string :slug, null: false, limit: 191
       t.integer :sluggable_id, null: false
       t.string :sluggable_type, limit: 50, null: false
       t.datetime :created_at
     end
 
-    add_index :simple_slug_history_slug, [:slug, :sluggable_type], unique: true
-    add_index :simple_slug_history_slug, [:sluggable_type, :sluggable_id]
+    add_index :simple_slug_history_slugs, :slug
+    add_index :simple_slug_history_slugs, [:sluggable_type, :sluggable_id], name: 'simple_slug_history_slugs_on_sluggable_type_and_sluggable_id'
   end
 end
